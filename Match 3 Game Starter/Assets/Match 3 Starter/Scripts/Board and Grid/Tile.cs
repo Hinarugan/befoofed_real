6c2fbd1072ebcc59a6e57245042d71ee1c0734e0
Hinarugan <chicatoy631@gmail.com>
2021-07-08 14:58:27 +0200

    MatchingTiles

--- Match 3 Game Starter/Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
+++ Match 3 Game Starter/Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
@@ -2,20 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Tile : MonoBehaviour {
+public class Tile : MonoBehaviour 
+{
 	private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
 	private static Tile previousSelected = null;
-
+	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
 	private SpriteRenderer render;
 	private bool isSelected = false;
+	private bool MatchFound = false;
 
 
 
-	void Awake() {
+	void Awake()
+	{
 		render = GetComponent<SpriteRenderer>();
     }
 
-	private void Select() {
+	private void Select()
+	{
 		isSelected = true;
 		render.color = selectedColor;
 		previousSelected = gameObject.GetComponent<Tile>();
@@ -44,17 +48,96 @@ public class Tile : MonoBehaviour {
 			}
 			else
 			{
-				SwapSprite(previousSelected.render);
-				previousSelected.Deselect(); 
+				if(GetAllAdjacentTiles().Contains(previousSelected.gameObject))
+					//Blickt die AdjacentTile Liste ein und überprüft ob das zuvor ausgewählte Tile in der AdjacentTile Liste ist.
+				{
+					SwapSprite(previousSelected.render);
+					previousSelected.ClearAllMatches();
+					previousSelected.Deselect();//Tauscht die ausgewählte Sprites miteinander.
+				}
+				else
+				{
+					previousSelected.GetComponent<Tile>().Deselect();
+					ClearAllMatches();
+					Select();//Die zweite gewählte Tile ist nicht neben der Ersten. Die erste Tile wird deselected, die Zweite wird selected.
+				}
 			}
 		}
 	}
 	public void SwapSprite(SpriteRenderer render2)
 	{
+
 		Sprite tempSprite = render2.sprite; 
 		render2.sprite = render.sprite;
 		render.sprite = tempSprite; 
 		
 	}
+	private GameObject GetAdjacent(Vector2 castDir)
+	{
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+		if(hit.collider != null)
+		{
+			return hit.collider.gameObject;
+		}
+		return null;
+	}//Die Methode nutzt den Raycast um die umliegenden Tiles zu identifizieren.
+
+	private List<GameObject> GetAllAdjacentTiles()
+	{
+		List<GameObject> AdjacentTiles = new List<GameObject>();
+		for (int i = 0; i < adjacentDirections.Length; i++)
+		{
+			AdjacentTiles.Add(GetAdjacent(adjacentDirections[i]));
+		}
+		return AdjacentTiles;
+	}
+	/*Diese Methode benutzt GetAdjacent() um eine Liste an umliegenden Tiles zu erzeugen. Loopt in alle Richtungen
+	und fügt alle angrenzenden Tiles zu adjacentDirections hinzu. Diese Methode verhindert, dass Tiles across the map geswappt
+	werden können.*/
+	private List<GameObject> FindMatch(Vector2 CastDir)
+		//amogus
+	{
+		List<GameObject> matchingTiles = new List<GameObject>();//Eine Liste wird erstellt, welche die MatchingTiles speichert.
+		RaycastHit2D Hit = Physics2D.Raycast(transform.position, CastDir);//Sendet Ray in die Richtung der CastDir
+		while (Hit.collider != null && Hit.collider.GetComponent<SpriteRenderer>().sprite == render.sprite)
+		{
+			matchingTiles.Add(Hit.collider.gameObject);
+			Hit = Physics2D.Raycast(Hit.collider.transform.position, CastDir);//Sendet weitere Raycasts bis entweder nichts getroffen wird oder
+			//der Sprite vom returnten Object Sprite abweicht. Wenn beide Voraussetzungen zutreffen wird es als Match registriert und der Liste hinzugefügt
+		}
+		return matchingTiles;
+	}
+	private void ClearMatch(Vector2[] paths)//Legt die Vector2 CastDir's als Array an
+	{
+		List<GameObject> matchingTiles = new List<GameObject>();//Liste, um Matching Tiles zu speichern.
+		for(int i = 0; i < paths.Length; i++)//Geht durch die Liste der CastDir's und fügt Matching Tiles der Liste hinzu.
+		{
+			matchingTiles.AddRange(FindMatch(paths[i]));
+		}
+		if (matchingTiles.Count >= 2)//Überprüft, ob eine Dreierreihe besteht.
+		{
+			for(int i = 0; i < matchingTiles.Count; i++)//Geht durch die matchingTiles und nullifizieren die Sprites.
+			{
+				matchingTiles[i].GetComponent<SpriteRenderer>().sprite = null;
+			}
+			MatchFound = true;
+		}
+			
+	}
+	public void ClearAllMatches()
+	{
+		if (render.sprite = null)
+		{
+			return;
+		}
+		ClearMatch(new Vector2[2] { Vector2.left, Vector2.right });
+		ClearMatch(new Vector2[2] { Vector2.up, Vector2.down });
+		if(MatchFound)
+		{
+			render.sprite = null;
+			MatchFound = false;
+		}
+
+	}//Checkt, ob das bestehende Match von rechts nach links oder von oben nach unten verläuft und löscht die entsprechenden Sprites.
 
 }
